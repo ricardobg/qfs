6e6ec7e9d0b7d926599f9456a009bed7ea6f978c
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2015-10-16 17:50:02 -0300

    Sending file

--- sender/src/qfs/sender/FileSender.java
+++ sender/src/qfs/sender/FileSender.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.Buffer;
 import java.util.concurrent.ArrayBlockingQueue;
 
@@ -14,11 +15,14 @@ public class FileSender {
 	//Circular multi-thread queue
 	private volatile boolean reading_finished = false;
 	private volatile boolean tcp_error = false;
-	private Exception reading_error = null;
+	private volatile boolean read_error = false;
+	private Exception reading_exception = null;
+	private Exception tcp_exception = null;
 	private ArrayBlockingQueue<byte[]> queue;
-	
-	private int queue_size = 100;
-	private int block_size = 10*1024;
+	private volatile int read_blocks = 0;
+	private volatile int sent_blocks = 0;
+	final private int queue_size = 100;
+	final private int block_size = 10*1024;
 	
 	public FileSender(String file_path) throws Exception {
 		file = new File(file_path);
@@ -28,7 +32,7 @@ public class FileSender {
 		queue = new ArrayBlockingQueue<byte[]>(queue_size);
 	}
 	
-	public void send(final String destination, final int port) {
+	public void send(final String destination, final int port) throws Exception {
 		//Reading thread
 		Thread read_thread = new Thread(){
 			@Override
@@ -39,16 +43,20 @@ public class FileSender {
 					fis = new FileInputStream(file);
 					while (!tcp_error && fis.read(buffer) > 0) {
 						queue.put(buffer);
+						read_blocks++;
 					}
 				}
 				catch (FileNotFoundException e) {
-					reading_error = new Exception("File '" + file + "' not found");
+					reading_exception = new Exception("File '" + file + "' not found");
+					read_error = true;
 				} catch (IOException e) {
 					// failed reading
-					reading_error = new Exception("I/O error: " + e.getMessage());
+					reading_exception = new Exception("I/O error: " + e.getMessage());
+					read_error = true;
 				} catch (InterruptedException e) {
 					// couldn't put buffer in queue
-					reading_error = new Exception("Sync error: " + e.getMessage());
+					reading_exception = new Exception("Sync error: " + e.getMessage());
+					read_error = true;
 				}
 				finally {
 					reading_finished = true;
@@ -64,20 +72,42 @@ public class FileSender {
 				try {
 					Socket socket = new Socket(destination, port);
 					DataOutputStream output = new DataOutputStream(socket.getOutputStream());
-					while (!queue.isEmpty() || !reading_finished) {
+					while ((!queue.isEmpty() || !reading_finished) && !read_error ) {
 						byte[] buffer = queue.take();
 						output.write(buffer);
+						output.flush();
+						sent_blocks++;
 					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					socket.close();
+				} catch (UnknownHostException e) {
+					tcp_exception = new Exception("Unkown host: " + e.getMessage());
+					tcp_error = true;
+				}
+				catch (IOException e) {
+					tcp_exception = new Exception("I/O error: " + e.getMessage());
+					tcp_error = true;
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					tcp_exception = new Exception("Sync error: " + e.getMessage());
+					tcp_error = true;
 				}
 			}
 		};
 		
-		
+		read_thread.start();
+		send_thread.start();
+		while (send_thread.isAlive() || read_thread.isAlive()) {
+			System.out.printf("\rRead Blocks: %5d. Sent Blocks: %5d", read_blocks, sent_blocks);
+			try {
+			    Thread.sleep(100);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}
+		}
+		//Finished loop, check for errors
+		if (read_error)
+			throw reading_exception;
+		if (tcp_error)
+			throw tcp_exception;
+		System.out.printf("\rRead Blocks: %5d. Sent Blocks: %5d", read_blocks, sent_blocks);
 	}
 }
