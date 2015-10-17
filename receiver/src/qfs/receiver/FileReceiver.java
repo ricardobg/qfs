35d4708c8b4b48b22e3c2975ba00fd167820dc58
Henrod <henriqrodrigues@gmail.com>
2015-10-17 12:00:50 -0300

    enxuto

--- receiver/src/qfs/receiver/FileReceiver.java
+++ receiver/src/qfs/receiver/FileReceiver.java
@@ -5,10 +5,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class FileReceiver {
@@ -21,8 +19,6 @@ public class FileReceiver {
 	final private int block_size = 100*1024;
 	private ArrayBlockingQueue<byte[]> buffer;
 	
-	private volatile boolean continueReading = true;
-	private String mutex = "";
 	public FileReceiver(String file_path, int port) {
 		file = new File(file_path);
 		buffer = new ArrayBlockingQueue<byte[]>(buffer_size);
@@ -31,89 +27,54 @@ public class FileReceiver {
 	}
 	
 	public void receive(final int port) {
-		
 		try {
 			serverSocket = new ServerSocket(port);
 			clientSocker = serverSocket.accept();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
 		
 		//Thread 4 -> write bytes to a file 
-				final Thread threadWriteFile = new Thread(){
-					@Override
-					public void run() {
-						super.run();
-						
-						FileOutputStream fos = null;
-						try {
-							fos = new FileOutputStream(file);
-						} catch (FileNotFoundException e1) {
-							e1.printStackTrace();
-						};
-						
-						while (true) {
-							synchronized (mutex) {
-								if (!continueReading && !buffer.isEmpty())
-									break;
-							}
-							try {
-								System.out.println("ola");
-								fos.write(buffer.take());
-								
-							} catch (FileNotFoundException e) {
-								e.printStackTrace();
-							} catch (IOException e) {
-								e.printStackTrace();
-							} catch (InterruptedException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}			
-						}
-						
-						try {
-							fos.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-						
-					}
-				};
+		final Thread threadWriteFile = new Thread(){
+			@Override
+			public void run() {
+				try {
+				super.run();
+				
+				FileOutputStream fos = null;
+					fos = new FileOutputStream(file);
+				
+				while (!clientSocker.isOutputShutdown()) {
+						System.out.println("ola");
+						byte[] b = buffer.take();
+						if(b.length > 0)
+							fos.write(b);
+						else break;
+									
+				}
+					fos.close();
+				} catch (IOException | InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		};
 		
 		//Thread 3 -> receive data and write the bytes in a buffer.
 		Thread threadWriteBuffer = new Thread() {
 			@Override
 			public void run(){
-				super.run();
-				
 				try {
+				super.run();
 					System.out.println("New connection with client " + clientSocker.getInetAddress().getHostAddress());
 					DataInputStream dis = new DataInputStream(clientSocker.getInputStream());
 					byte[] b = new byte[block_size];
-					do
-					{
-						int read = 0;
-						synchronized (mutex) {
-							read = dis.read(b);
-							if (read == 0)
-								continueReading = false;
-						}
-						if (read > 0) {
-							buffer.put(b);
-						}
-						else
-							break;
-					}
-					while (true);
 					
+					while (dis.read(b) > 0) {
+						buffer.put(b);
+					}					
+					
+					buffer.put(new byte[0]);
 					
 					serverSocket.close();
 					clientSocker.close();
-				} catch (IOException e) {
-					System.out.println("Fail to access port.");
-					e.printStackTrace();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
+				} catch(IOException | InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
@@ -123,29 +84,11 @@ public class FileReceiver {
 		threadWriteFile.start();
 		
 		while (threadWriteBuffer.isAlive() || threadWriteFile.isAlive()) {
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	private void readBytesToBuffer(InputStream is) {
-		Scanner scanner = new Scanner(is);
-		byte[] sequence_bytes = new byte[block_size];
-		
-		for (int i = 0; scanner.hasNextByte(); i++) {
-			sequence_bytes[i] = scanner.nextByte();
+			Thread.sleep(100);
 		}
 		
-		try {
-			buffer.put(sequence_bytes);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		scanner.close();
 	}
 }
